28b30193447f13f99663d66b8e08d4922a891daf
mycognosist <gnomad@cryptolab.net>
2023-05-26 12:58:55 +0200

    add standalone jsonrpc config struct

--- solar/src/actors/jsonrpc/config.rs
+++ solar/src/actors/jsonrpc/config.rs
@@ -1,4 +1,23 @@
-// Define the default IP used for the JSON-RPC server.
-pub const JSONRPC_IP: &str = "127.0.0.1";
-// Define the default port used for the JSON-RPC server.
-pub const JSONRPC_PORT: u16 = 3030;
+use std::net::{IpAddr, Ipv4Addr};
+
+#[derive(Debug, Clone)]
+pub struct JsonRpcConfig {
+    /// Run the JSON-RPC server (default: true).
+    pub server: bool,
+
+    /// IP to bind for JSON-RPC server (default: 127.0.0.1).
+    pub ip: IpAddr,
+
+    /// Port to bind for JSON-RPC server (default: 3030).
+    pub port: u16,
+}
+
+impl Default for JsonRpcConfig {
+    fn default() -> Self {
+        Self {
+            server: true,
+            ip: IpAddr::V4(Ipv4Addr::new(127, 0, 0, 1)),
+            port: 3030,
+        }
+    }
+}

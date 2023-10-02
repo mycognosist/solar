5db338737a0c9693e4f585d6df749a71b935fba9
mycognosist <gnomad@cryptolab.net>
2023-10-02 12:01:45 +0200

    add follows endpoint

--- solar/src/actors/jsonrpc/server.rs
+++ solar/src/actors/jsonrpc/server.rs
@@ -183,6 +183,29 @@ pub async fn actor(server_id: OwnedIdentity, server_addr: SocketAddr) -> Result<
         })
     })?;
 
+    // Retrieve the public keys of all feeds followed by the given public key.
+    //
+    // Returns an array of public keys.
+    rpc_module.register_method("follows", move |params: Params, _| {
+        task::block_on(async {
+            let pub_key: PubKey = params.parse()?;
+
+            let db = KV_STORE.read().await;
+
+            if let Some(indexes) = &db.indexes {
+                let follows = indexes.get_follows(&pub_key.0)?;
+                let response = json!(follows);
+
+                Ok::<Value, JsonRpcError>(response)
+            } else {
+                let empty_vec: Vec<String> = Vec::new();
+                let response = json!(empty_vec);
+
+                Ok::<Value, JsonRpcError>(response)
+            }
+        })
+    })?;
+
     // Retrieve the public keys of all feeds subscribed to the given channel.
     //
     // Returns an array of public keys.

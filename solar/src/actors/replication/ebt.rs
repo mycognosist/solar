36305c9b4eaf614622e5a304e17456a0270f5079
mycognosist <gnomad@cryptolab.net>
2023-10-06 15:19:45 +0200

    add ebt state struct and associated methods

--- solar/src/actors/replication/ebt.rs
+++ solar/src/actors/replication/ebt.rs
@@ -10,86 +10,173 @@
 // EBT in ScuttleGo:
 //
 // https://github.com/planetary-social/scuttlego/commit/e1412a550652c791dd97c72797ed512f385669e8
-
 // http://dev.planetary.social/replication/ebt.html
-
-// Each name is a JSON string which represents a feed in the @ character notation.
-// The validity of this string should be confirmed when a note is received.
-// Receiving a note with a malformed feed identifier should terminate the EBT
-// session with an error.
-//type SsbId = String;
-
-// Each value is a JSON number. The number can be negative, positive or a zero.
-// Receiving a note with a malformed value should terminate the EBT session
-// with an error.
-//type Value = i64;
-
-// Each note is a JSON object containing one or more name/value pairs.
-//type Note: HashMap<SsbId, Value>;
 
 // serde_json::to_string(&notes)?;
 // "{\"@FCX/tsDLpubCPKKfIrw4gc+SQkHcaD17s7GI6i/ziWY=.ed25519\":123,\"@l1sGqWeCZRA99gN+t9sI6+UOzGcHq3KhLQUYEwb4DCo=.ed25519\":-1}"
 
-use std::convert::TryInto;
+use std::{collections::HashMap, convert::TryInto};
 
-/// Decode a value from a control message (aka. note), returning the values
-/// of the replicate flag, receive flag and sequence.
-///
-/// If the replicate flag is `false`, the peer does not wish to receive
-/// messages for the referenced feed.
-///
-/// If the replicate flag is `true`, values will be returned for the receive
-/// flag and sequence. The sequence refers to a sequence number of the
-/// referenced feed.
-fn decode(value: i64) -> (bool, Option<bool>, Option<u64>) {
-    let (replicate_flag, receive_flag, sequence) = if value < 0 {
-        // Replicate flag is `false`.
-        // Peer does not wish to receive messages for this feed.
-        (false, None, None)
-    } else {
-        // Get the least-significant bit (aka. rightmost bit).
-        let lsb = value & 1;
-        // Set the receive flag value.
-        let receive_flag = if lsb == 0 { true } else { false };
-        // Perform a single bit arithmetic right shift to obtain the sequence
-        // number.
-        let sequence: u64 = (value >> 1).try_into().unwrap();
+use crate::{config::PEERS_TO_REPLICATE, node::KV_STORE, Result};
 
-        (true, Some(receive_flag), Some(sequence))
-    };
+/// An SSB identity in the form `@...=.ed25519`.
+// The validity of this string should be confirmed when a note is received.
+// Receiving a note with a malformed feed identifier should terminate the EBT
+// session with an error.
+type SsbId = String;
+/// The encoded vector clock value.
+// Receiving a note with a malformed value should terminate the EBT session
+// with an error.
+type EncodedClockValue = i64;
 
-    (replicate_flag, receive_flag, sequence)
+/// A vector clock which maps an SSB ID to an encoded vector clock value.
+type VectorClock = HashMap<SsbId, EncodedClockValue>;
+
+struct Ebt {
+    /// The SSB ID of the local node.
+    id: SsbId,
+    /// Duration to wait before switching feed request to a different peer.
+    timeout: u16,
+    /// The local vector clock.
+    local_clock: VectorClock,
+    /// The vector clock for each known peer.
+    peer_clocks: HashMap<SsbId, VectorClock>,
 }
 
-/// Encode a replicate flag, receive flag and sequence number as a control
-/// message (aka. note) value.
-///
-/// If the replicate flag is `false`, a value of `-1` is returned.
-///
-/// If the replicate flag is `true` and the receive flag is `true`, a single
-/// bit arithmetic left shift is performed on the sequence number and the
-/// least-significant bit is set to `0`.
-///
-/// If the replicate flag is `true` and the receive flag is `false`, a single
-/// bit arithmetic left shift is performed on the sequence number and the
-/// least-significant bit is set to `1`.
-fn encode(replicate_flag: bool, receive_flag: Option<bool>, sequence: Option<u64>) -> i64 {
-    if replicate_flag {
-        // Perform a single bit arithmetic left shift.
-        let mut signed: i64 = (sequence.unwrap() << 1).try_into().unwrap();
-        // Get the least-significant bit (aka. rightmost bit).
-        let lsb = signed & 1;
-        // Set the least-significant bit based on the value of the receive flag.
-        if let Some(_flag @ true) = receive_flag {
-            // Set the LSB to 0.
-            signed |= 0 << lsb;
-        } else {
-            // Set the LSB to 1.
-            signed |= 1 << lsb;
+impl Default for Ebt {
+    fn default() -> Self {
+        Ebt {
+            id: String::new(),
+            timeout: 3000,
+            local_clock: HashMap::new(),
+            peer_clocks: HashMap::new(),
         }
-        signed
-    } else {
-        -1
+    }
+}
+
+impl Ebt {
+    // Read peer clock state from file.
+    // fn load_peer_clocks()
+    // Write peer clock state to file.
+    // fn persist_peer_clocks()
+
+    async fn init() -> Result<Self> {
+        let mut ebt = Ebt::default();
+
+        // Get list of peers to replicate.
+        if let Some(peers) = PEERS_TO_REPLICATE.get() {
+            // Request replication of each peer.
+            for peer in peers.keys() {
+                ebt.request(peer).await?;
+            }
+        }
+        // TODO: Load peer clocks from file and update `peer_clocks`.
+
+        Ok(ebt)
+    }
+
+    /// Retrieve the vector clock for the given SSB ID.
+    fn get_clock(self, peer_id: &SsbId) -> Option<VectorClock> {
+        if peer_id == &self.id {
+            Some(self.local_clock)
+        } else {
+            self.peer_clocks.get(peer_id).cloned()
+        }
+    }
+
+    /// Set or update the vector clock for the given SSB ID.
+    fn set_clock(&mut self, peer_id: &SsbId, clock: VectorClock) {
+        if peer_id == &self.id {
+            self.local_clock = clock
+        } else {
+            self.peer_clocks.insert(peer_id.to_owned(), clock);
+        }
+    }
+
+    /// Request that the feed represented by the given SSB ID be replicated.
+    async fn request(&mut self, peer_id: &SsbId) -> Result<()> {
+        // Look up the latest sequence for the given ID.
+        if let Some(seq) = KV_STORE.read().await.get_latest_seq(peer_id)? {
+            // Encode the replicate flag, receive flag and sequence.
+            let encoded_value: EncodedClockValue = Ebt::encode(true, Some(true), Some(seq))?;
+            // Insert the ID and encoded sequence into the local clock.
+            self.local_clock.insert(peer_id.to_owned(), encoded_value);
+        }
+
+        Ok(())
+    }
+
+    /// Revoke a replication request for the feed represented by the given SSB
+    /// ID.
+    fn revoke_request(&mut self, peer_id: &SsbId) {
+        self.local_clock.remove(peer_id);
+    }
+
+    /// Decode a value from a control message (aka. note), returning the values
+    /// of the replicate flag, receive flag and sequence.
+    ///
+    /// If the replicate flag is `false`, the peer does not wish to receive
+    /// messages for the referenced feed.
+    ///
+    /// If the replicate flag is `true`, values will be returned for the receive
+    /// flag and sequence. The sequence refers to a sequence number of the
+    /// referenced feed.
+    fn decode(value: i64) -> Result<(bool, Option<bool>, Option<u64>)> {
+        let (replicate_flag, receive_flag, sequence) = if value < 0 {
+            // Replicate flag is `false`.
+            // Peer does not wish to receive messages for this feed.
+            (false, None, None)
+        } else {
+            // Get the least-significant bit (aka. rightmost bit).
+            let lsb = value & 1;
+            // Set the receive flag value.
+            let receive_flag = lsb == 0;
+            // Perform a single bit arithmetic right shift to obtain the sequence
+            // number.
+            let sequence: u64 = (value >> 1).try_into()?;
+
+            (true, Some(receive_flag), Some(sequence))
+        };
+
+        Ok((replicate_flag, receive_flag, sequence))
+    }
+
+    /// Encode a replicate flag, receive flag and sequence number as a control
+    /// message (aka. note) value.
+    ///
+    /// If the replicate flag is `false`, a value of `-1` is returned.
+    ///
+    /// If the replicate flag is `true` and the receive flag is `true`, a single
+    /// bit arithmetic left shift is performed on the sequence number and the
+    /// least-significant bit is set to `0`.
+    ///
+    /// If the replicate flag is `true` and the receive flag is `false`, a single
+    /// bit arithmetic left shift is performed on the sequence number and the
+    /// least-significant bit is set to `1`.
+    fn encode(
+        replicate_flag: bool,
+        receive_flag: Option<bool>,
+        sequence: Option<u64>,
+    ) -> Result<i64> {
+        let value = if replicate_flag {
+            // Perform a single bit arithmetic left shift.
+            let mut signed: i64 = (sequence.unwrap() << 1).try_into()?;
+            // Get the least-significant bit (aka. rightmost bit).
+            let lsb = signed & 1;
+            // Set the least-significant bit based on the value of the receive flag.
+            if let Some(_flag @ true) = receive_flag {
+                // Set the LSB to 0.
+                signed |= 0 << lsb;
+            } else {
+                // Set the LSB to 1.
+                signed |= 1 << lsb;
+            }
+            signed
+        } else {
+            -1
+        };
+
+        Ok(value)
     }
 }
 
@@ -111,14 +198,13 @@ mod test {
         VALUES
             .iter()
             .zip(NOTES)
-            .for_each(|(value, note)| assert_eq!(decode(*value), note));
+            .for_each(|(value, note)| assert_eq!(Ebt::decode(*value).unwrap(), note));
     }
 
     #[test]
     fn test_encode() {
-        VALUES
-            .iter()
-            .zip(NOTES)
-            .for_each(|(value, note)| assert_eq!(encode(note.0, note.1, note.2), *value));
+        VALUES.iter().zip(NOTES).for_each(|(value, note)| {
+            assert_eq!(Ebt::encode(note.0, note.1, note.2).unwrap(), *value)
+        });
     }
 }
